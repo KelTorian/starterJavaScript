66c6ebbd0fb0b1df847a9a14417a1cbde7727a5a
TorianKel <77552336+KelTorian@users.noreply.github.com>
2021-10-23 14:50:36 +0300

    test

--- src/app.js
+++ src/app.js
@@ -10,7 +10,30 @@ for (let i = 0; i < konutKredileri.length; i++) {
   console.log("<li>" + konutKredileri[i] + "</li>");
 }
 
-//go go
-//go go go
-//go go go go
-//ole ole ole
+let save = function () {
+  console.log("hello");
+};
+
+save();
+
+let students = ["ahmet", "mehmet", "kerim"];
+
+console.log(students);
+
+console.log(Math.max(...[5, 3, 4, 15, 45]));
+
+let populations = [10000, 20000, 30000, [40000, 50000]];
+
+// let [small, medium, large, [veryHigh, max]] = populations;
+
+// console.log(small);
+// console.log(medium);
+// console.log(large);
+// console.log(veryHigh);
+// console.log(max);
+
+function someFunction([mahmut, mehmet], number) {
+  console.log(mahmut, mehmet);
+}
+
+someFunction(populations);
