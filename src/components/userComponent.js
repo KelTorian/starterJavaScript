a0a2aeaaf029f2eaaee803300fdc53fcdbe6c231
Torian Kel <toriankel@gmail.com>
2021-11-22 23:53:44 +0300

    22.11.2021 Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -3,6 +3,7 @@ import {
   ElasticLogger,
   MongoLogger,
 } from "../crossCuttingConcerns/logging/logger.js";
+import Customer from "../models/customer.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
 
@@ -13,8 +14,19 @@ let userService = new UserService(logger1);
 let user1 = new User(1, "Torian", "Kel", "İstanbul");
 let user2 = new User(2, "Mahmut", "Kerim", "Adana");
 
-userService.add(user1);
-userService.add(user2);
+//userService.add(user1);
+//userService.add(user2);
 
-console.log(userService.getById(1));
-console.log(userService.list());
+//console.log(userService.getCustomerById(1));
+//console.log(userService.listCustomers());
+
+console.log("--------------------------------");
+userService.load();
+
+// let customerToAdd = new Customer(1, "Cemil", "Meriç", "Urfa", 80);
+// customerToAdd.type = "customer";
+//userService.add(customerToAdd);
+console.log(userService.customers);
+console.log(userService.employees);
+console.log(userService.errors);
+console.log(userService.getCustomersSorted());
